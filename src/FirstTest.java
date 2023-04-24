d17cd1700aa164e8a826aa0c678a800d2f42c00b
vera <katwer@mail.ru>
2023-04-24 15:24:15 +0400

    create testCheckSavingSecondArticleInFavorite

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -403,6 +403,141 @@ public class FirstTest {
         );
     }
 
+    @Test
+    public void testCheckSavingSecondArticleInFavorite() {
+        waitForElementAndClick(
+                By.xpath("//*[@text='Search Wikipedia']"),
+                "Cannot find Search line on Main screen",
+                5
+        );
+        String searchInput = "Java";
+        waitForElementAndSendKey(
+                By.xpath("//*[@text='Search…']"),
+                searchInput,
+                "Cannot find input search",
+                5
+        );
+        String firstArticle = "Java (programming language)";
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='"+ firstArticle +"']"),
+                "Cannot find" + firstArticle + " after input " + searchInput + " in Search line",
+                5
+        );
+        waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find article title",
+                15
+        );
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageView[@content-desc='More options']"),
+                "Cannot find More options button on screen",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//android.widget.TextView[@text='Add to reading list']"),
+                "Cannot find Add to reading list in menu",
+                5
+        );
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/onboarding_button"),
+                "Cannot find button Got it on onbording",
+                5
+        );
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/text_input"),
+                "Cannot find input to set name of article",
+                5
+        );
+        String titleOfList = "Learning programming";
+        waitForElementAndSendKey(
+                By.id("org.wikipedia:id/text_input"),
+                titleOfList,
+                "Cannot put text in input Name of this list",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@text='OK']"),
+                "Cannot find button OK",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Cannot find X button",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@text='Search Wikipedia']"),
+                "Cannot find Search line on Main screen",
+                5
+        );
+        waitForElementAndSendKey(
+                By.xpath("//*[@text='Search…']"),
+                searchInput,
+                "Cannot find input search",
+                5
+        );
+        String secondArticle = "JavaScript";
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='"+ secondArticle +"']"),
+                "Cannot find" + secondArticle +" after input " + searchInput+ "in Search line",
+                5
+        );
+        waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find article title",
+                15
+        );
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageView[@content-desc='More options']"),
+                "Cannot find More options button on screen",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@text='Add to reading list']"),
+                "Cannot find Add to reading list in menu",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@text='"+ titleOfList +"']"),
+                "Cannot find favorite list " + titleOfList,
+                15
+        );
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Cannot find X button",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//android.widget.FrameLayout[@content-desc='My lists']"),
+                "Cannot find My list in navigation menu",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@text='"+ titleOfList + "']"),
+                "Cannot find ny list " + titleOfList,
+                5
+        );
+        swipeElementToLeft(
+                By.xpath("//*[@text='" + secondArticle + "']"),
+                "Cannot find saved article"
+        );
+        waitForElementNotPresent(
+                By.xpath("//*[@text='" + secondArticle + "']"),
+                "Still see article in list",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@text='"+ firstArticle + "']"),
+                "Cannot find article" + firstArticle,
+                15
+        );
+        assertElementHasText(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                firstArticle,
+                "Title of article doesn't match"
+        );
+    }
+
     private WebElement assertElementHasText(By by, String expected, String errorMessage) {
         WebElement textElement = waitForElementPresent(by, errorMessage);
         String titleText = textElement.getAttribute("text");
@@ -410,7 +545,6 @@ public class FirstTest {
                 errorMessage,
                 expected,
                 titleText
-
         );
         return textElement;
     }
